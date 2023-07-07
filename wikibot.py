9473efa9f541c6fa75d2c581a1208bbb41c6f244
eyamao <51378081+eyamao@users.noreply.github.com>
2023-07-07 00:33:40 +0000

    style improvements

--- wikibot.py
+++ wikibot.py
@@ -4,7 +4,7 @@ import click
 @click.command()
 @click.option('--name', prompt="Wikipedia page to scrape", help='Web page to scrape')
 @click.option('--length', default=1, prompt='Length of the output', help='Length of the requested output')
-def cli(name,length):
+def cli(name='Microsoft',length=1):
     result = scrape(name, length=length)
     click.echo(click.style(f"{result}:", fg="white", bg="blue"))
 

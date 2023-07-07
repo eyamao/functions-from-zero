f5ff2ee254aee86d7d68afda397a05e225c1e87d
eyamao <51378081+eyamao@users.noreply.github.com>
2023-07-07 01:58:09 +0000

    Fix format issues

--- wikibot.py
+++ wikibot.py
@@ -1,15 +1,19 @@
 from mylib.bot import scrape
 import click
 
+
 @click.command()
-@click.option('--name', prompt="Wikipedia page to scrape", help='Web page to scrape')
-@click.option('--length', default=1, prompt='Length of the output', help='Length of the requested output')
-def cli(name='Microsoft',length=1):
+@click.option("--name", prompt="Wikipedia page to scrape", help="Web page to scrape")
+@click.option(
+    "--length",
+    default=1,
+    prompt="Length of the output",
+    help="Length of the requested output",
+)
+def cli(name="Microsoft", length=1):
     result = scrape(name, length=length)
     click.echo(click.style(f"{result}:", fg="white", bg="blue"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
-
-
-
